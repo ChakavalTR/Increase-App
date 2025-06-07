13daa10bd0ab8dcc30a9328dcec8d51518f1f3e5
TR Val <ChakavalTR@yahoo.com>
2025-06-07 09:05:13 +0700

    more code

--- lib/home_view.dart
+++ lib/home_view.dart
@@ -8,10 +8,66 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
-  int _counter = 0;
+  var _counter = 0;
+  var currentIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            UserAccountsDrawerHeader(
+              decoration: BoxDecoration(color: Colors.blue),
+              accountName: Text(
+                'TR Val',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+              ),
+              accountEmail: Text(
+                'ChakavalTR@yahoo.com',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+              ),
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: NetworkImage(
+                  'https://avatars.githubusercontent.com/u/123456789?v=4',
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.home, color: Colors.blue),
+              title: Text('Home', style: TextStyle(fontSize: 16)),
+              trailing: Icon(Icons.arrow_forward, color: Colors.blue),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.notifications, color: Colors.blue),
+              title: Text('Notifications', style: TextStyle(fontSize: 16)),
+              trailing: Icon(Icons.arrow_forward, color: Colors.blue),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.person, color: Colors.blue),
+              title: Text('Profile', style: TextStyle(fontSize: 16)),
+              trailing: Icon(Icons.arrow_forward, color: Colors.blue),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.logout, color: Colors.blue),
+              title: Text('Logout', style: TextStyle(fontSize: 16)),
+              trailing: Icon(Icons.arrow_forward, color: Colors.blue),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
       appBar: AppBar(
         title: Text(
           'Increase App',
@@ -19,6 +75,27 @@ class _HomeViewState extends State<HomeView> {
         ),
         centerTitle: true,
         backgroundColor: Colors.blue,
+        actions: [
+          Stack(
+            children: [
+              IconButton(
+                icon: Icon(Icons.notifications, color: Colors.white, size: 30),
+                onPressed: () {},
+              ),
+              Positioned(
+                right: 8,
+                child: CircleAvatar(
+                  radius: 8,
+                  backgroundColor: Colors.red,
+                  child: Text(
+                    '3',
+                    style: TextStyle(color: Colors.white, fontSize: 12),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
       body: Center(
         child: Column(
@@ -67,6 +144,30 @@ class _HomeViewState extends State<HomeView> {
             ),
           ],
         ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        onTap: (index) {
+          setState(() {
+            currentIndex = index;
+          });
+        },
+        currentIndex: currentIndex,
+        selectedItemColor: Colors.blue,
+        unselectedItemColor: Colors.grey,
+        showUnselectedLabels: true,
+        showSelectedLabels: true,
+        selectedIconTheme: IconThemeData(size: 28),
+        unselectedIconTheme: IconThemeData(size: 22),
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Search'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+        ],
       ),
     );
   }
